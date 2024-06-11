f0e9bcdca4716f4640134a3bb3494f3eb9de9e68
Pierre Meli <pierre.meli@vivasync.ch>
2024-06-11 13:23:22 +0200

    neue Applikation mit Eingabemöglichkeit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          StockInfo App by Pierre
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [ticker, setTicker] = useState<string>('ADS.EU');
+    const [stockData, setStockData] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const exampleParams = {
+        "cmd": "getStockData",
+        "params": {
+            "ticker": ticker,
+            "lang": "en"
+        }
+    };
+
+    const getStockData = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get('https://tradernet.com/api/', {
+                params: { q: JSON.stringify(exampleParams) }
+            });
+            setStockData(response.data);
+        } catch (error) {
+            console.error('Fehler beim Abrufen der Aktieninformationen:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Stock Info</h1>
+            <div>
+                <input 
+                    type="text" 
+                    value={ticker} 
+                    onChange={(e) => setTicker(e.target.value)} 
+                    placeholder="Ticker eingeben"
+                />
+                <button onClick={getStockData}>Suche</button>
+            </div>
+            <div id="stock-data">
+                {loading ? (
+                    <p>Lade Daten...</p>
+                ) : stockData ? (
+                    <div>
+                        <h3>Stock Data für {stockData.ticker}</h3>
+                        <p>Preis: {stockData.quotes['pp']}</p>
+                        <p>Währung: {stockData.quotes['x_curr']}</p>
+                        <p>Änderung: {stockData.quotes['chg110']}</p>
+                    </div>
+                ) : (
+                    <p>Keine Daten verfügbar</p>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
